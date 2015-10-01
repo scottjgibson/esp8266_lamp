130e1fbbc438c4773faab21f30548d4164bd242e
Scott Gibson <scottgibson@gmail.com>
2015-09-30 20:58:39 -0400

    initial platform.io setup

---
+++ config_sample.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H 1
+
+// This is the sensor definition field. Please comment *in* the 
+// Sensor that you have installed.
+//#define SENSOR_DHT22 1
+#define SENSOR_DS18S20 1
+
+const char* ssid = "****";
+const char* password = "****";
+const char* hostname = "roomsensor";
+
+#endif /* CONFIG_H */
