73711bd5e9299f8b2651d88176bf367273721b5d
Scott Gibson <scottgibson@gmail.com>
2015-09-30 21:52:32 -0400

    intial dimmer code

--- config_sample.h
+++ config_sample.h
@@ -1,13 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H 1
 
-// This is the sensor definition field. Please comment *in* the 
-// Sensor that you have installed.
-//#define SENSOR_DHT22 1
-#define SENSOR_DS18S20 1
 
 const char* ssid = "****";
 const char* password = "****";
-const char* hostname = "roomsensor";
+const char* hostname = "light";
 
 #endif /* CONFIG_H */
--- src/main.cpp
+++ src/main.cpp
@@ -4,163 +4,44 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-#ifdef SENSOR_DS18S20
-#include <OneWire.h>
-#define ONE_WIRE_BUS 2  // DS18S20 pin
-OneWire ds(ONE_WIRE_BUS);
-#endif
+const int buttonPin = 2;           // number of the button pin
+const int ledPin = 0;              // number of the LED pin
+int buttonState = 0;               // button state starts at 0
+int brightness = 0;                // brightness of LED starts at 0
+int fadeAmount = 25.50;            // fadeAmount is equal to 10% of 255(max LED brightness)
+int brighness = 0;            // fadeAmount is equal to 10% of 255(max LED brightness)
+int buttonPressedCounter = 0;    // timer set to 0 whe depressing button
+int buttonCounter = 0;             // button counter set to 0
+int buttonLastState = 0;           // button last state set to 0
 
-#ifdef SENSOR_DHT22
-#include <DHT.h>
-#include "../config.h"
-#define DHTTYPE DHT22
-#define DHTPIN  2
-DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
-#endif
 
 MDNSResponder mdns;
 ESP8266WebServer server(80);
 
 
-float humidity, temp;  // Values read from sensor
 String webString="";     // String to display
 // Generally, you should use "unsigned long" for variables that hold time
 unsigned long previousMillis = 0;        // will store last temp was read
 const long interval = 2000;             // interval at which to read sensor
 
-bool ICACHE_FLASH_ATTR gettemperature() {
+bool ICACHE_FLASH_ATTR gettemperature()
+{
   // Wait at least 2 seconds seconds between measurements.
   // if the difference between the current time and last time you read
   // the sensor is bigger than the interval you set, read the sensor
   // Works better than delay for things happening elsewhere also
   unsigned long currentMillis = millis();
  
-  if(currentMillis - previousMillis >= interval) {
+  if(currentMillis - previousMillis >= interval)
+  {
     // save the last time you read the sensor 
     previousMillis = currentMillis;
-
-#ifdef SENSOR_DS18S20
-		// This is the code for receiving temperature readings from a DS18S20.
-		// see https://github.com/esp8266/Arduino/blob/esp8266/libraries/OneWire/examples/DS18x20_Temperature/DS18x20_Temperature.pde
-		byte i;
-		byte present = 0;
-		byte type_s;
-		byte data[12];
-		byte addr[8];
-
-		ds.reset();
-		ds.reset_search();
-		if ( !ds.search(addr)) {
-			Serial.println("No more addresses.");
-			Serial.println();
-			ds.reset_search();
-			delay(250);
-			return false;
-		}
-
-		//Serial.print("ROM =");
-		//for( i = 0; i < 8; i++) {
-		//	Serial.write(' ');
-		//	Serial.print(addr[i], HEX);
-		//}
-
-		if (OneWire::crc8(addr, 7) != addr[7]) {
-			Serial.println("CRC is not valid!");
-			return false;
-		}
-		//Serial.println();
-
-		// the first ROM byte indicates which chip
-		switch (addr[0]) {
-			case 0x10:
-				//Serial.println("  Chip = DS18S20");  // or old DS1820
-				type_s = 1;
-				break;
-			case 0x28:
-				//Serial.println("  Chip = DS18B20");
-				type_s = 0;
-				break;
-			case 0x22:
-				//Serial.println("  Chip = DS1822");
-				type_s = 0;
-				break;
-			default:
-				Serial.println("Device is not a DS18x20 family device.");
-				return false;
-		} 
-
-		ds.reset();
-		ds.select(addr);
-		ds.write(0x44, 0);        // start conversion, no parasitic power
-
-		delay(750);     // maybe 750ms is enough, maybe not
-		// we might do a ds.depower() here, but the reset will take care of it.
-
-		present = ds.reset();
-		ds.select(addr);
-		ds.write(0xBE);         // Read Scratchpad
-
-		//Serial.print("  Data = ");
-		//Serial.print(present, HEX);
-		//Serial.print(" ");
-		for ( i = 0; i < 9; i++) {           // we need 9 bytes
-			data[i] = ds.read();
-			//Serial.print(data[i], HEX);
-			//Serial.print(" ");
-		}
-		//Serial.print(" CRC=");
-		//Serial.print(OneWire::crc8(data, 8), HEX);
-		//Serial.println();
-
-		// Convert the data to actual temperature
-		// because the result is a 16 bit signed integer, it should
-		// be stored to an "int16_t" type, which is always 16 bits
-		// even when compiled on a 32 bit processor.
-		int16_t raw = (data[1] << 8) | data[0];
-		if (type_s) {
-			raw = raw << 3; // 9 bit resolution default
-			if (data[7] == 0x10) {
-				// "count remain" gives full 12 bit resolution
-				raw = (raw & 0xFFF0) + 12 - data[6];
-			}
-		} else {
-			byte cfg = (data[4] & 0x60);
-			// at lower res, the low bits are undefined, so let's zero them
-			if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-			else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-			else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-			//// default is 12 bit resolution, 750 ms conversion time
-		}
-		temp = (float)raw / 16.0;
-		//Serial.print("Temperature: ");
-		//Serial.println(temp);
-#endif // of DS18S20-related code
-
-#ifdef SENSOR_DHT22 // Read temp&hum from DHT22
-		// Reading temperature for humidity takes about 250 milliseconds!
-		// Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
-		humidity = dht.readHumidity();          // Read humidity (percent)
-		temp = dht.readTemperature(false);     // Read temperature as Celsius
-#endif
-
-		//Serial.print("Free heap:");
-		//Serial.println(ESP.getFreeHeap(),DEC);
-
-		if (isnan(temp) || temp==85.0 || temp==(-127.0)) {
-			Serial.println("Failed to read from sensor");
-			// resetting the previous measurement time so that a failed attempt
-			// will be repeated with the next query.
-			previousMillis=currentMillis-2000;
-			if (previousMillis < 0) previousMillis = 0;
-			return false;
-		} else {
-			return true;
-		}
 	}
 }
 
 
-void ICACHE_FLASH_ATTR handleNotFound(){
+void ICACHE_FLASH_ATTR handleNotFound()
+{
 	String message = "File Not Found\n\n";
 	message += "URI: ";
 	message += server.uri();
@@ -169,7 +50,8 @@ void ICACHE_FLASH_ATTR handleNotFound(){
 	message += "\nArguments: ";
 	message += server.args();
 	message += "\n";
-	for (uint8_t i=0; i<server.args(); i++){
+	for (uint8_t i=0; i<server.args(); i++)
+  {
 		message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
 	}
 	server.send(404, "text/plain", message);
@@ -182,7 +64,8 @@ void ICACHE_FLASH_ATTR setup(void){
 	Serial.println("Wifi temperature sensor v0.1");
 
 	// Wait for connection
-	while (WiFi.status() != WL_CONNECTED) {
+	while (WiFi.status() != WL_CONNECTED)
+  {
 		delay(500);
 		Serial.print(".");
 	}
@@ -193,54 +76,78 @@ void ICACHE_FLASH_ATTR setup(void){
 	Serial.print("IP address: ");
 	Serial.println(WiFi.localIP());
 
-	if (mdns.begin(hostname, WiFi.localIP())) {
+	if (mdns.begin(hostname, WiFi.localIP()))
+  {
 		Serial.println("MDNS responder started");
 	}
 
-	server.on("/", [](){
-			if (gettemperature()) {       // read sensor
-			webString = "Sensor " + String(hostname) + " reports:\n";
-			webString+="Temperature: "+String(temp)+" degree Celsius\n";
-#ifdef SENSOR_DHT22 // Read temp&hum from DHT22
-			webString+="Humidity: "+String(humidity)+" % r.H.\n";
-#endif
-			server.send(200, "text/plain", webString);            // send to someones browser when asked
-			} else {
-			webString="{\"error\": \"Cannot read data from sensor.\"";
-			server.send(503, "text/plain", webString);            // send to someones browser when asked
-			}
-			});
-	server.on("/temperature", [](){
-			if (gettemperature()) {       // read sensor
-			webString="{\"temperature\": "+String(temp)+",\"unit\": \"Celsius\"}";
-			server.send(200, "text/plain", webString);            // send to someones browser when asked
-			} else {
-			webString="{\"error\": \"Cannot read data from sensor.\"";
-			server.send(503, "text/plain", webString);            // send to someones browser when asked
-			}
-			});
-#ifdef SENSOR_DHT22 // Read humidity from DHT22
-	server.on("/humidity", [](){
-			if (gettemperature()) {       // read sensor
-			gettemperature();       // read sensor
-			webString="{\"humidity\": "+String(humidity)+",\"unit\": \"% r.H.\"}";
-			server.send(200, "text/plain", webString);            // send to someones browser when asked
-			} else {
-			webString="{\"error\": \"Cannot read data from sensor.\"";
-			server.send(503, "text/plain", webString);            // send to someones browser when asked
-			}
-			});
-
-#endif
+	server.on("/", []()
+  {
+    if (gettemperature())
+    {       // read sensor
+      webString = String(hostname) + " reports:\n";
+      webString+="Brightness: "+String(0)+" \n";
+      server.send(200, "text/plain", webString);            // send to someones browser when asked
+    }
+    else 
+    {
+      webString="{\"error\": \"Cannot read data from sensor.\"";
+      server.send(503, "text/plain", webString);            // send to someones browser when asked
+    }
+  });
 
 	server.onNotFound(handleNotFound);
 
 	server.begin();
 	ESP.wdtEnable(5000);
+
+  // set button as input
+  pinMode(buttonPin, INPUT);
+  // set LED as output
+  pinMode(ledPin, OUTPUT);
 }
 
 void ICACHE_FLASH_ATTR loop(void){
 	server.handleClient();
 	ESP.wdtFeed();
+
+  buttonState = digitalRead(buttonPin);
+  
+  // activate when button is being held down
+  if (buttonState == HIGH)
+  {
+    buttonPressedCounter ++; // start counter
+    Serial.print("Pressed Timer: ");
+    Serial.println(buttonPressedCounter);
+    // if counter equals 100 turn off LED and reset counter to 0
+    if (buttonPressedCounter == 50)
+    {
+      buttonCounter = 0;
+      analogWrite(ledPin, 0); // reset LED 
+    }
+  } 
+  else 
+  {
+    // on button depress, set counter to 0
+    buttonPressedCounter = 0; 
+  }
+  
+  // activate once to increase button counter, which adjusts the LED brightness
+  if (buttonState != buttonLastState)
+  {
+    if (buttonState == HIGH)
+    {
+      // increase counter only if less than 10
+      if (buttonCounter < 10)
+      {
+        buttonCounter ++;
+      }
+      Serial.print("LED Level: ");
+      Serial.println(buttonCounter);
+      brightness = buttonCounter * fadeAmount;
+      analogWrite(ledPin, brightness); // adjust LED brightness to buttoncounter * fadeamount
+    }   
+    buttonLastState = buttonState;
+  }
 }
 
